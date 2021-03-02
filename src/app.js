772acbc219e3bd4901ed23a32c961a99c248b5fe
«Meastrix001» <Nroofthooft2012@gmail.com>
2021-03-02 16:02:33 +0100

    created quiz flow

--- src/app.js
+++ src/app.js
@@ -32,7 +32,11 @@ const app = () => {
     initialize() {
       this.cacheElements();
       this.registerListeners();
-      this.currentQuestion = 0
+      this.currentQuestion = 0;
+      this.questionWrong = 0;
+      this.questionRight = 0;
+      this.questionSkip = 0;
+      this.myAnswers = [];
     },
     cacheElements() {
       this.$createQuizeForm = document.getElementById('form');
@@ -40,11 +44,24 @@ const app = () => {
       this.$quizDifficulty = document.querySelector('.difficulty')
       this.$quizTags = document.querySelector('.tags')
       this.$quizAmount = document.querySelector('.amount')
-      this.$quizQuestions = document.getElementById('quiz-questions')
+      this.$quizQuestions = document.querySelector('.quiz-questions')
+      this.$quizArea = document.querySelector('quiz-area')
       this.$quizAnswers = document.querySelector('.quiz-answers')
       this.$quizCounter = document.querySelector('.counter')
       this.$nextQuestion = document.querySelector('.nextQuestion')
+      this.$skipQuestion = document.querySelector('.skipQuestion')
       this.$answerList = document.querySelector('.answerList')
+      this.$preQuizHeader = document.querySelector('.preQuiz-header')
+      this.$startNewQuiz = document.querySelector('.restart-quiz')
+      this.$startNewQuizEnd = document.querySelector('.restart-quiz-end')
+      this.$clearGameArea = document.querySelectorAll('.clear')
+      this.$results = document.querySelector('.results')
+      this.$resultsList = document.querySelector('.results-list')
+      this.$quizBody = document.querySelector('.quiz-body')
+      this.$startNewQuiz.style.display = "none"
+      this.$nextQuestion.style.display = "none"
+      this.$skipQuestion.style.display = "none"
+      this.$results.style.display = "none"
     },
     registerListeners() {
       this.$createQuizeForm.addEventListener('click', (e) => {
@@ -55,16 +72,49 @@ const app = () => {
         console.log(this.$quizTags.value)
         console.log(this.$quizAmount.value)
         console.log("-------------")
+        this.$nextQuestion.style.display = "block"
+        this.$startNewQuiz.style.display = "block"
+        this.$skipQuestion.style.display = "block"
         this.fetchQuizzAPI()
       })
-      this.$nextQuestion.addEventListener('click', (e) => {
+      this.$skipQuestion.addEventListener('click', (e) => {
         e.preventDefault()
         this.currentQuestion = this.currentQuestion + 1
         this.createQuizQuestions(this.quizQuestionArray)
         this.questionSkipped()
       })
+      this.$startNewQuiz.addEventListener('click', (e) => {
+        this.restartQuiz()
+        this.$startNewQuiz.style.display = "none"
+        this.$nextQuestion.style.display = "none"
+        this.$skipQuestion.style.display = "none"
+      })
+      this.$startNewQuizEnd.addEventListener('click', (e) => {
+        this.restartQuiz()
+        this.$startNewQuiz.style.display = "none"
+        this.$nextQuestion.style.display = "none"
+        this.$skipQuestion.style.display = "none"
+      })
+      this.$nextQuestion.addEventListener("click", (e) => {
+        this.checkAnswer(this.answerID, this.correctAnswer)
+        this.createQuizQuestions(this.quizQuestionArray)
+      })
+    },
+    restartQuiz(){
+      this.currentQuestion = 0
+      this.questionWrong = 0
+      this.questionRight = 0
+      this.questionSkip = 0
+      this.myAnswers = [];
+      this.$preQuizHeader.style.display = "flex"
+      this.$quizBody.style.display = "block"
+      this.$results.style.display = "none"
+      this.$clearGameArea.forEach(clear => {
+        clear.innerHTML = ""
+      })
     },
     async fetchQuizzAPI() {
+
       //example URL
       // https://quizapi.io/api/v1/questions?apiKey=YOUR_API_KEY&category=linux&difficulty=Easy&limit=10&tags=HTML,JavaScript
 
@@ -72,41 +122,68 @@ const app = () => {
       await fetch(`${QUIZZ_API}?apiKey=${QUIZZ_TOKEN}&category=${this.$quizCategory.value}&difficulty=${this.$quizDifficulty.value}&limit=${this.$quizAmount.value}`)
         .then(response => response.json())
         .then(json => {
+          console.log(json)
           this.quizQuestionArray = json
           this.createQuizQuestions(this.quizQuestionArray)
         })
         .catch(error => console.log(error))
     },
     createQuizQuestions(data) {
-      this.$quizQuestions.innerHTML = ""
-      this.$quizQuestions.innerHTML += `<li><p>${data[this.currentQuestion].question}</p></li>`
-      this.$quizCounter.innerHTML = `<p>${this.currentQuestion}/${data.length}</p>`
-      this.createHTMLForAnswers(data)
+      if (this.currentQuestion !== data.length){
+        this.$quizQuestions.innerHTML = ""
+        this.$preQuizHeader.style.display = "none"
+        this.$quizQuestions.innerHTML += `<li><p>${data[this.currentQuestion].question}</p></li>`
+        this.$quizCounter.innerHTML = `<p class="wrapper">Questions skipped: ${this.questionSkip}/${data.length} || Question Right: ${this.questionRight}/${data.length} || Question Wrong: ${this.questionWrong}/${data.length} || Quiz Length: ${this.currentQuestion}/${data.length} || </p> <p id="timer"></p>`
+        this.createHTMLForAnswers(data);
+      } else {
+        this.quizEnd(data);
+      }
     },
     createHTMLForAnswers(data) {
+      this.createTimer()
       this.$quizAnswers.innerHTML = ""
       this.correctAnswer = data[this.currentQuestion].correct_answer
       console.log(data[this.currentQuestion])
       console.log(`correct answer${data[this.currentQuestion].correct_answer}`)
       for (var key in data[this.currentQuestion].answers) {
         if (data[this.currentQuestion].answers.hasOwnProperty(key) && data[this.currentQuestion].answers[key] !== null) {
-          console.log(key + " -> " + data[this.currentQuestion].answers[key]);
-          this.$quizAnswers.innerHTML += `<li data-answer="${key}" class="answer_card">${data[this.currentQuestion].answers[key]} <br> ${key}</li>`
+          this.$quizAnswers.innerHTML += `<li data-answer="${key}" class="answer_card container">
+          <h2>${data[this.currentQuestion].answers[key]}</h2>
+          <h3>${key}</h3></li>`
         }
       }
 
       this.$selectedAnswer = document.querySelectorAll('.answer_card')
       this.$selectedAnswer.forEach((answer) => {
         answer.addEventListener('click', (e) => {
-          let answerID = e.target.dataset.answer
-          this.checkAnswer(answerID, this.correctAnswer)
-          this.createQuizQuestions(this.quizQuestionArray)
+         this.answerID = e.target.dataset.answer
+          console.log(this.answerID)
+            e.target.classList.toggle("active")
         })
       })
+    },
+    createTimer(){
+      let timeLeft = 15;
+      let elem = document.getElementById('timer');
+      let timerId = setInterval(countdown, 1000);
+      
+      function countdown() {
+          if (timeLeft === 0) {
+            this.questionSkipped()
+            clearTimeout(timerId);
+          } else {
+              elem.innerHTML = `Time Left!: ${timeLeft}`;
+              timeLeft--;
+          }
+      }
+      
+
     },
     checkAnswer(answerID, data){
       console.log(answerID)
       this.currentQuestion = this.currentQuestion + 1
+      console.log(this.myAnswers)
+      this.myAnswers.push(answerID)
       if (answerID === data){
         this.questionCorrect()
       } else {
@@ -115,15 +192,34 @@ const app = () => {
     },
     questionCorrect(){
       console.log("CORRECT")
-      this.$answerList.innerHTML += `<h2>CORRECT</h2>`
+      this.$answerList.innerHTML += `<h2>${this.currentQuestion}. CORRECT</h2>`
+      this.questionRight = this.questionRight + 1
     },
     questionwrong(){
       console.log("Better luck next time!")
-      this.$answerList.innerHTML += `<h2>WRONG</h2>`
+      this.$answerList.innerHTML += `<h2>${this.currentQuestion}. WRONG</h2>`
+      this.questionWrong = this.questionWrong + 1
     },
     questionSkipped(){
       console.log("Question Skipped")
-      this.$answerList.innerHTML += `<h2>SKIPPED</h2>`
+      this.$answerList.innerHTML += `<h2>${this.currentQuestion}. SKIPPED</h2>`
+      this.questionSkip = this.questionSkip + 1
+    },
+    quizEnd(data){ 
+      this.$quizBody.style.display = "none"
+      this.myAnswers.map(answer => {
+        this.$results.style.display = "block"
+        let endGame = data.map(question => {
+          return `<li style="list-style=none">
+          <h2> The Question: ${question.question}</h2>
+          <h3>The correct answer: ${question.correct_answer}</h3>
+          <p> your answer: ${answer}</p>
+          </li> `
+        }).join(' ')
+        this.$resultsList.innerHTML = `<ul>
+        ${endGame}
+        </ul>`
+      })
     }
   }
   app.initialize()
